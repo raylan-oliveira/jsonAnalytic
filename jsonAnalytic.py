b58c9854840f6a3a44f4cf0f8f3d9ef36c8b5906
Raylan Oliveira <ray14gt@gmail.com>
2022-08-08 12:55:06 -0500

    add Feature _cache.json

--- jsonAnalytic.py
+++ jsonAnalytic.py
@@ -3,17 +3,29 @@ import concurrent.futures
 import argparse
 import re
 import sys
+from urllib.parse import urlparse
 from collections import deque
-from os.path import exists
 fila = deque()
+from os.path import exists
 
 import json
-from time import sleep
+all_dicts = []
+def criar_json(name_file_json, dados):
+	if create_cache_global == 'true':
+		name_file = f'{name_file_json}_cache.json'
+		if exists(name_file) == False:		
+			with open(name_file, 'a', encoding='utf8') as f:
+				
+				json.dump(dados, f, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ':'))
+				print(f'[{name_file}] create!')
+		else:
+			print(f'[{name_file}] exist... Not created!')
 
+from time import sleep
 from os import mkdir
 
 def dict_or_list(name_file,list_all, test_isinstance, all_keys_or_keys_search):
-	
+	global all_dicts
 	if isinstance(test_isinstance, dict):	
 		
 		# for parse keys
@@ -31,6 +43,12 @@ def dict_or_list(name_file,list_all, test_isinstance, all_keys_or_keys_search):
 				if dict_armazenar not in list_all:
 					print(test_isinstance[key])
 					list_all.append(dict_armazenar)
+			
+			if create_cache_global == 'true':
+				dict_armazenar = {}
+				dict_armazenar[key] = test_isinstance[key]				
+				all_dicts.append(dict_armazenar)
+				
 			if isinstance(test_isinstance[key], dict) or isinstance(test_isinstance[key], list):
 				if test_isinstance[key] not in fila:
 					fila.append(test_isinstance[key])
@@ -56,6 +74,12 @@ def dict_or_list(name_file,list_all, test_isinstance, all_keys_or_keys_search):
 						if dict_armazenar not in list_all:	
 							print(item[key])
 							list_all.append(dict_armazenar)
+					
+					if create_cache_global == 'true':
+						dict_armazenar = {}
+						dict_armazenar[key] = item[key]						
+						all_dicts.append(dict_armazenar)
+						
 					if isinstance(item[key], dict) or isinstance(item[key], list):
 						if item[key] not in fila:
 							fila.append(item[key])
@@ -79,9 +103,23 @@ def dict_or_list(name_file,list_all, test_isinstance, all_keys_or_keys_search):
 								dict_armazenar[key] = item_list[key]
 								if dict_armazenar not in list_all:	
 									print(item_list[key])
-									list_all.append(dict_armazenar)									
+									list_all.append(dict_armazenar)
+							
+							if create_cache_global == 'true':
+								dict_armazenar = {}
+								dict_armazenar[key] = item_list[key]
+								all_dicts.append(dict_armazenar)
 							
 	return list_all	
+def read_json_cache(json_file,is_search):
+	#json_fila = list
+	for dic in json_file:
+		for key in dic:
+			if is_search:
+				if key in is_search:
+					print(dic[key])
+			else:
+				print(key)
 	
 def show_all_keys(name_file,js):
 	list_all_keys = []
@@ -107,14 +145,16 @@ def ler_json(file):
 		with open(file, 'r', encoding='utf8') as f:
 			return json.load(f)
 	except Exception as e:
-		print(f'Error read file: {file}\n\tError: {e}')
+		print(f'Error read file: {file}\n\t{e}')
 		return 0
 
 def file_or_url(argv, line_stdin, js):
-	if len(argv) == 1:
+	
+	if len(argv) == 2:
 		
 		if js != 0:
-			print(f'File: {line_stdin} - All Keys:')
+			print(f'Input: {line_stdin} - All Keys:')
+			
 			show_all_keys(line_stdin, js)
 		else:
 			pass
@@ -139,12 +179,17 @@ def file_or_url(argv, line_stdin, js):
 					cont += 1
 			else:
 				pass
+create_cache_global = 'true'
 if __name__ == "__main__":	
-
-	if '-f' not in sys.argv and '--file' not in sys.argv and '--help' not in sys.argv and '-h' not in sys.argv:
+	name_file_cache = '_file'
+	if '-p' in sys.argv or '-P' in sys.argv or '-pipe' in sys.argv or '--PIPE' in sys.argv:
+		##stdin
 		
+		name_file_cache = '_stdin'
 		for line_stdin in sys.stdin:
 			
+			if line_stdin == '':
+				continue
 			line_stdin = line_stdin.replace('\n', '')
 			# download url with json
 			if re.search(r'^http', line_stdin):
@@ -159,27 +204,72 @@ if __name__ == "__main__":
 			else:
 				js = ler_json(line_stdin)
 				file_or_url(sys.argv, line_stdin, js)
-	
+		
+		if '-c' in sys.argv or '--create_cache' in sys.argv:
+			create_cache_global = 'false'
+		
+		if create_cache_global == 'true':
+			criar_json(name_file_cache, all_dicts)	
 	else:
+		#argparse
+			
 		parser = argparse.ArgumentParser(description='jsonAnalytic - List all keys & all values in json')
-		parser.add_argument("-f", "--file", help='File .json', required=True, type=str)
+		
+		parser.add_argument("-i", "--input_", help='File .json or URL with json response', required=True, type=str)
+		
 		parser.add_argument('-k', '--keys_search', help='keys search', required=False, type=str, default=None,  nargs='+')
-		parser.add_argument('-a' , '--all_keys', help='show all keys', required=False, type=bool, default=True)
+		parser.add_argument('-c' , '--create_cache', help='Create *_cache.json', required=False, type=str, default='true')
 		
-		args = parser.parse_args()			
+		args = parser.parse_args()
 		
-		js = ler_json(args.file)
-		if js != 0: 
+		create_cache_global = args.create_cache		
+		
+		if re.search(r'^http', args.input_):
+			url_parse_all_info = urlparse( args.input_)
+	
+			netloc = url_parse_all_info.netloc
+			name_file_cache = '_' + netloc
+			headers = {				
+					'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+			}
+			
+			js = requests.get(args.input_, headers=headers)
 			
 			if args.keys_search != None:
 				
-				name_file = args.file
-				show_all_values(name_file,js, args.keys_search)
+				name_file = args.input_
+				show_all_values(name_file,js.json(), args.keys_search)
 			else:
-				name_file = args.file
-				show_all_keys(name_file,js)
+				
+				name_file = args.input_
+				
+				show_all_keys(name_file,js.json())
 		else:
-			print(f'[{args.file}] - key not found')
-			
-			
+			js = ler_json(args.input_)
+			name_file_cache = '_' + args.input_.replace('.json', '')
+			if js != 0: 
+				if re.search(r'\_cache\.json$', args.input_):
+					create_cache_global = 'false'
+					read_json_cache(js, args.keys_search)
+					
+				else:
+					if args.keys_search != None:
+						
+						name_file = args.input_
+						show_all_values(name_file,js, args.keys_search)
+					else:
+						name_file = args.input_
+						show_all_keys(name_file,js)
+				
+			else:
+				print(f'[{args.input_}] - key not found')
 		
+		new_name_file_cache = ''
+		if args.keys_search:
+			for key in args.keys_search:
+				
+				new_name_file_cache = new_name_file_cache + '_' + key
+			
+			name_file_cache = new_name_file_cache
+		if args.create_cache == 'true':
+			criar_json(name_file_cache, all_dicts)
